8b4f47a51e5b76b71adc1ad3f1d0ea3a2ea12336
Devlee <hjcloud@me.com>
2022-04-27 20:05:41 +0900

    add stompHandler

--- src/main/java/com/sparta/demo/signal/config/WebSocketConfig.java
+++ src/main/java/com/sparta/demo/signal/config/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package com.sparta.demo.signal.config;
 
+import com.sparta.demo.signal.handler.StompHandler;
 import com.sparta.demo.signal.socket.SocketHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocket
@@ -16,3 +18,39 @@ public class WebSocketConfig implements WebSocketConfigurer {
                 .setAllowedOrigins("*");
     }
 }
+//@RequiredArgsConstructor
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//
+//    private final StompHandler stompHandler;
+//
+////    @Override
+////    public void configureMessageBroker(MessageBrokerRegistry config) {
+////        config.enableSimpleBroker("/sub");
+////        config.setApplicationDestinationPrefixes("/pub");
+////    }
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+//        registry.addEndpoint("/ws-stomp")
+//                .setAllowedOriginPatterns("*")
+////        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*").setAllowedOriginPatterns("*")
+//                .withSockJS() // sock.js를 통하여 낮은 버전의 브라우저에서도 websocket 이 동작할수 있게 합니다.
+//                .setHeartbeatTime(2000);
+//    }
+//
+//
+//    @Override
+//    public void configureClientInboundChannel (ChannelRegistration registration){
+//        registration.interceptors(stompHandler);
+//    }
+//
+//    @Override
+//    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+//        registration.setMessageSizeLimit(160 * 64 * 1024); // default : 64 * 1024
+//        registration.setSendTimeLimit(100 * 10000); // default : 10 * 10000 60 * 10000 * 6
+//        registration.setSendBufferSizeLimit(3* 512 * 1024); // default : 512 * 1024
+//    }
+//}
