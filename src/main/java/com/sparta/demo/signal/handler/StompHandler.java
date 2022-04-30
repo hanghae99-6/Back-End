402db223ce7da7e4d685d369f813d565f01a8c62
msk102948 <msk102948@gmail.com>
2022-05-01 04:56:18 +0900

    [feat] openvidu 커스터마이징 / 영상용 stomp 편집
    ** 오류 있음!! 실행불가 **
    Description:
    
    Parameter 3 of constructor in com.sparta.demo.signal.handler.StompHandler required a bean of type 'com.sparta.demo.model.StpMessage' that could not be found.
    
    Action:
    
    Consider defining a bean of type 'com.sparta.demo.model.StpMessage' in your configuration.

--- src/main/java/com/sparta/demo/signal/handler/StompHandler.java
+++ src/main/java/com/sparta/demo/signal/handler/StompHandler.java
@@ -1,5 +1,10 @@
 package com.sparta.demo.signal.handler;
 
+import com.sparta.demo.model.Debate;
+import com.sparta.demo.model.StpMessage;
+import com.sparta.demo.repository.DebateRepository;
+import com.sparta.demo.repository.StpMessageRepository;
+import com.sparta.demo.service.DebateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
@@ -10,7 +15,6 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
-import java.util.logging.Logger;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,6 +27,10 @@ public class StompHandler implements ChannelInterceptor {
 //    private final RoomRepository roomRepository;
 //    private final UserRepository userRepository;
 //    private final EnterUserRepository enterUserRepository;
+    private final DebateService debateService;
+    private final DebateRepository debateRepository;
+    private final StpMessageRepository stpMessageRepository;
+    private final StpMessage stpMessage;
     private final Long min = 0L;
 
 
@@ -39,10 +47,56 @@ public class StompHandler implements ChannelInterceptor {
             log.info("if StompCommand.SUBSCRIBE");
             log.info("message.getHeaders().get('simpDestination') : {}",message.getHeaders().get("simpDestination"));
             log.info("accessor.getSessionId : {}", accessor.getSessionId());
+
+            String name = "name With hardcoding";
+            String roomId = debateService.getRoomId( Optional.ofNullable((String)message.getHeaders().get("simpDestination")).orElse("InvalidPartitionId"));
+//            String sessionId = (String) message.getHeaders().get("simpSessionId");
+
+            try {
+                debateService.sendChatMessage(StpMessage.builder().type(StpMessage.MessageType.ENTER).roomId(roomId).videoUser(name).build());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // TODO: 여기를 partitionId라고 따로 만들어야할지, rommId라고 둬야할지 잘 모르겠어요. message에 들어있는 simpDestination이 백에서 지정해준 roomId가 맞는지 모르겠어요.
+            if (roomId != null) {
+                Debate debate = debateRepository.findByRoomId(roomId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 방입니다."));
+//                room.setUserCount(redisRepository.getUserCount(roomId));
+//                if (redisRepository.getUserCount(roomId) < 0) {
+//                    room.setUserCount(min);
+//                }
+                debateRepository.save(debate);
+            }
+
         }else if (StompCommand.DISCONNECT == accessor.getCommand()) {
             log.info("if StompCommand.DISCONNECT");
-            log.info("message.getHeaders().get('simpDestination') : {}",message.getHeaders().get("simpDestination"));
+            log.info("message.getHeaders().get('simpDestination') : {}", message.getHeaders().get("simpDestination"));
+
+            String name = "name With hardcoding - out";
+            String roomId = debateService.getRoomId(Optional.ofNullable((String) message.getHeaders().get("simpDestination")).orElse("InvalidPartitionId"));
+//            String sessionId = (String) message.getHeaders().get("simpSessionId");
+
+            if (roomId != null) {
+
+
+                try {
+                    debateService.sendChatMessage(StpMessage.builder().type(StpMessage.MessageType.QUIT).roomId(roomId).videoUser(name).build());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+//
+                Debate debate = debateRepository.findByRoomId(roomId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 방입니다.(DISCONNECT)"));
+
+                stpMessage.setUserCount(stpMessageRepository.findAllByRoomId(roomId) - 1);
+            }
         }
+        return message;
+
+
+
+
+
+
 
 //        // websocket 연결시 헤더의 jwt token 검증
 //        if (StompCommand.CONNECT == accessor.getCommand()) {
@@ -120,6 +174,6 @@ public class StompHandler implements ChannelInterceptor {
 //                }
 //            }
 //        }
-        return message;
+//        return message;
     }
 }
