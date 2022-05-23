fc8f4863d1c001e04fe7c117ad17eda0b216c563
whyyougd <holygd73@naver.com>
2022-05-24 03:17:35 +0900

    [feat] 토론방에 상대자가 들어오지 않았을 경우 상세페이지에 빈값 보내주기

--- src/main/java/com/sparta/demo/service/MainService.java
+++ src/main/java/com/sparta/demo/service/MainService.java
@@ -111,6 +111,15 @@ public class MainService {
         debate.setVisitCnt(debate.getVisitCnt()+1L);
 
         List<EnterUser> enterUserList = enterUserRepository.findByDebate_DebateIdOrderBySideDesc(debateId);
+        // 토론방에 상대자가 들어오지 않았을 경우 상세페이지에 빈값 보내주기
+        if(enterUserList.size()<2){
+            EnterUser enterUser = new EnterUser();
+            if(enterUserList.get(0).getSide().getTypeNum()==1)
+                enterUserList.add(enterUser);
+            else
+                enterUserList.add(0,enterUser);
+        }
+
         return ResponseEntity.ok().body(new MainDetailResponseDto(debate, enterUserList, side));
     }
 
