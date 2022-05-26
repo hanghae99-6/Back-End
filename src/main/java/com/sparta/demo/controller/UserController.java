82e9928cd95fa739421d6d1a3ebee8c0aac82b42
whyyougd <holygd73@naver.com>
2022-05-26 19:11:17 +0900

    [style] import 정리

--- src/main/java/com/sparta/demo/controller/UserController.java
+++ src/main/java/com/sparta/demo/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import javax.jws.soap.SOAPBinding;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
