95950f2222e44957dcdaca4f3d624435585ea336
msk102948 <msk102948@gmail.com>
2022-05-28 18:46:19 +0900

    [fix] redis expireKey

--- src/main/java/com/sparta/demo/service/SessionService.java
+++ src/main/java/com/sparta/demo/service/SessionService.java
@@ -205,9 +205,9 @@ public class SessionService {
 
     private void saveDebate(Debate debate){
         log.info("saveDebate 진입");
-        HashOperations<String , String, StatusTypeEnum> hashOperations = redisTemplate.opsForHash();
+        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
         String redisKey = String.valueOf(debate.getDebateId());
-        hashOperations.put(redisKey, DEBATE_STATUS, debate.getStatusEnum());
+        hashOperations.put(redisKey, DEBATE_STATUS, debate.getStatusEnum().getName());
         log.info("저장 된 값 확인: {}", hashOperations.get(redisKey, DEBATE_STATUS));
         redisTemplate.expire(redisKey, DEFAULT_TIMEOUT, TimeUnit.HOURS);
     }
