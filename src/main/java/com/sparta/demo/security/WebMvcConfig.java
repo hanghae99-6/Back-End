c9659d7e9800711038394786647963911cc2f103
Devlee <hjcloud@me.com>
2022-05-30 13:37:00 +0900

    [fix] 쿠키 필터 삭제

--- src/main/java/com/sparta/demo/security/WebMvcConfig.java
+++ src/main/java/com/sparta/demo/security/WebMvcConfig.java
@@ -3,7 +3,6 @@ package com.sparta.demo.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseCookie;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
