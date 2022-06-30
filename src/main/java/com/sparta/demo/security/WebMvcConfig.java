a90e2906d7bd3fae01952e704d080419687caa9f
Devlee <hjcloud@me.com>
2022-06-30 19:14:34 +0900

    [fix] CORS 수정

--- src/main/java/com/sparta/demo/security/WebMvcConfig.java
+++ src/main/java/com/sparta/demo/security/WebMvcConfig.java
@@ -15,7 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //                .allowedOrigins("http://localhost:3000")
                 .allowedOrigins("https://wepeech.com")
                 .allowedOrigins("https://www.wepeech.com")
-//                .allowedOrigins("*")
+                .allowedOrigins("*")
                 .allowedHeaders("*")
                 .exposedHeaders(HttpHeaders.AUTHORIZATION)
                 .allowedMethods(
